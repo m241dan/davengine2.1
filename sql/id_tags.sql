5a1ea05568f9bf3824d0e9d48d2ede8518c85a4a
Davenge <korisd@gmail.com>
2015-06-23 11:13:56 -0400

    actually tracking id tags seperately from the data, like I should have done the wholetime

---
+++ sql/id_tags.sql
@@ -0,0 +1,15 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `id_tags`;
+CREATE TABLE IF NOT EXISTS `id_tags` (
+   `type` smallint NOT NULL DEFAULT '-1',
+   `id` int NOT NULL DEFAULT '-1',
+   `can_recycle` smallint NOT NULL DEFAULT '0',
+   `created_by` varchar(256) NOT NULL DEFAULT 'system',
+   `created_on` varchar(256) NOT NULL DEFAULT ' ',
+   `modified_by` varchar(256) NOT NULL DEFAULT 'system',
+   `modified_on` varchar(256) NOT NULLD DEFAULT ' ',
+   PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `id_tags` ADD UNIQUE `unique_index` ( `type`, `id` );
