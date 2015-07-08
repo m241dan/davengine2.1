25009a81e4e01fc0cc68f20ed1a64381f6d68be2
Davenge <korisd@gmail.com>
2015-07-08 15:00:35 -0400

    put in a really neat and dynamic way to control commands for accountControl, will need a more complicated one for entities

--- scripts/controls/accountControl.lua
+++ scripts/controls/accountControl.lua
@@ -1,6 +1,27 @@
+local global_account, global_input
+
+account_command = {}
+
 function onInterp( account, input )
+   global_account = account
+   global_input = input
+   local value
+   local command = account_command[input]
+   if( command ~= nil and command[2] <= account:getLevel() ) then
+      value = command[1]()
+      if( value ~= nil ) then
+         account:msg( string.format( "%s", value ) )
+      end
+   else
+      account:msg( "Bad command, try again.\r\n" )
+   end
 end
 
 function onPrompt( account )
    return "<>"
 end
+
+account_command["hi"] = { function()
+   global_account:msg( "Hi there.\r\n" ) 
+   return "lets shorten this a bit\r\n"
+end, 0 }
