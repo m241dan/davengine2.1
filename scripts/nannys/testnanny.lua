d3bb147408f96987cace2c53c052efbf57116cd1
Davenge <korisd@gmail.com>
2015-07-20 11:49:08 -0400

    added the test nanny

---
+++ scripts/nannys/testnanny.lua
@@ -0,0 +1,8 @@
+function onNannyLoad( nanny )
+   print( "This is a test" )
+   if( nanny == nil ) then
+      print( "nanny is nil" )
+   else
+      print( "nanny is not nil (o.o)b" )
+   end
+end
