dd29aedd4df374d277dc6fdec93e037fe1f3c691
Davenge <korisd@gmail.com>
2015-07-27 14:33:43 -0400

    added a global interpreter for handling general style interprets

--- scripts/controls/accountControl.lua
+++ scripts/controls/accountControl.lua
@@ -1,37 +1,10 @@
+require( "scripts/libs/interpreter_lib" )
 require( "scripts/libs/string_lib" )
-
-local global_account = nil
-local global_input = nil
 
 account_command = {}
 
 function onInterp( account, input )
-   local value, cmd, command, found
-   global_account = account
-
-   cmd, input = oneArg( input )
-   command = account_command[cmd]
-   global_input = input
-   
-   if( command == nil ) then
-      for i, command in pairs( account_command ) do
-         if( isPrefix( i, cmd ) == true ) then
-            found = true
-            break
-         end
-      end
-      if( found == false )
-         account:msg( "Bad command, try again.\r\n" )
-         return
-      end
-   end
-   if( command[2] <= account:getLevel() ) then
-      value = command[1]()
-      if( value ~= nil ) then
-         account:msg( string.format( "%s", value ) )
-      end
-   else
-
+   davInterp( account, account_command, input )
 end
 
 function onPrompt( account )
@@ -57,16 +30,16 @@ function onPrompt( account )
    return prompt
 end
 
-account_command["create a character"] = { function()
+account_command["create a character"] = { function( account, input )
    local nanny = Nanny.new( "scripts/nannys/nannyCharCreate.lua" )
-   local socket = Socket.get( global_account )  
+   local socket = Socket.get( account )  
    local control_index = socket:control( nanny )
    socket:setState( control_index )
    nanny:setState( 0 )
    return nil
 end, 0 }
 
-account_command["hi"] = { function()
-   global_account:msg( "Hi there.\r\n" ) 
+account_command["hi"] = { function( account, input )
+   account:msg( "Hi there.\r\n" ) 
    return nil
 end, 0 }
---
+++ scripts/libs/interpreter_lib.lua
@@ -0,0 +1,26 @@
+function davInterp( object, table, input )
+   local value, cmd, command
+
+   cmd, input = oneArg( input )
+   command = table[cmd]
+
+   if( command == nil ) then
+      for i, v in pairs( table ) do
+         if( isPrefix( i, cmd ) == true ) then
+            command = v
+            break
+         end
+      end
+   end
+
+   if( command == nil or command[2] > object:getLevel() ) then
+      object:msg( "Bad command, try again.\r\n" )
+      return
+   end
+
+   value = command[1]( object, input )
+   if( value ~= nil ) then
+      account:msg( value )
+   end
+
+end
